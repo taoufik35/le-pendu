8f13d21290e4ecc7482d45080e0a561a289f8d67
taoufik <aitdra.taoufik@gmail.com>
2021-03-04 23:35:48 +0100

    welcome message and word choise

---
+++ js/projet_du_pendu.js
@@ -0,0 +1,10 @@
+alert("Bonjour et Bienvenue au jeu du pendu");
+
+var motATouver = ["cour","cheval","pierre", "centre"];
+var lettre = [
+    ["c","o","u","r" ]
+    ["c","h","e","v","a","l"],
+    ["p","i","e","r","r","e"],
+    ["c","e","n","t","r","e"]
+];
+
