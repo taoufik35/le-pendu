02d5b63eaa2489ace5f06775b8b422617fda89c2
taoufik <aitdra.taoufik@gmail.com>
2021-03-07 17:15:44 +0100

    function choose word and compare user lettre

--- js/projet_du_pendu.js
+++ js/projet_du_pendu.js
@@ -21,4 +21,28 @@ victoire = false;
 nb_essais_restans = essais;
 lettre_Restantes = alphabet;
 
+function choix_du_mot(){
+    let tab = vocabulaire.length;
+    let nombre = Math.random() * (tab);
+    nombre = Math.round(nombre);
+    mot_a_deviner = vocabulaire[nombre];
+ 
+}
+choix_du_mot();
+
+function compare (lettre, mot){
+    var sortie;
+    for(i = 0 ; i < mot.length;i++){
+        if(mot[i]==lettre){
+            sortie=true;
+            break;
+        
+        }
+        else {
+            sortie = false;
+        }
+    }
+          return sortie;
+}
+
 
