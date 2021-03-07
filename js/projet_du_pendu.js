204ebd92c4e2219f94d5ea599a844b2e8e929afe
taoufik <aitdra.taoufik@gmail.com>
2021-03-07 15:59:59 +0100

    other variables and start game functions

--- js/projet_du_pendu.js
+++ js/projet_du_pendu.js
@@ -5,3 +5,20 @@ let alphabet = "abcdefghijklmnopqrstuvwxyz";
 let essais = 7;
 let lettre_Restantes = 'abcdefghijklmnopqrstuvwxyz';
 
+//--------------------Autres variable---------------------------------
+
+let mot_a_deviner;
+let nb_essais_restans;
+let jeu_en_cours;
+let victoire;
+let affichage;
+let lettre_joouee;
+
+//------------------------LES FONCTIONS-------------------------------
+//========================Début de partie=============================
+jeu_en_cours = true;
+victoire = false;
+nb_essais_restans = essais;
+lettre_Restantes = alphabet;
+
+
